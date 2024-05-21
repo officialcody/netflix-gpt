a55bfc1cc8d47adca98211b83c1bfa3be8e53028
Sparsh Goel <sparshgoelt26@gmail.com>
2024-05-21 21:57:05 +0530

    firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwKbz6Wo1UVp1BzKSJVXV6hTz3vwy74k8",
-  authDomain: "netflixgpt-57a56.firebaseapp.com",
-  projectId: "netflixgpt-57a56",
-  storageBucket: "netflixgpt-57a56.appspot.com",
-  messagingSenderId: "1093133802935",
-  appId: "1:1093133802935:web:c284961959cce2a54adc32",
-  measurementId: "G-DEHFXGZ999",
+  apiKey: "AIzaSyDO2SJeLRA9HUfv_0Ho0GAy5KS1KhR60f0",
+  authDomain: "netfilx-gpt-1ab2c.firebaseapp.com",
+  projectId: "netfilx-gpt-1ab2c",
+  storageBucket: "netfilx-gpt-1ab2c.appspot.com",
+  messagingSenderId: "6879908163",
+  appId: "1:6879908163:web:1da2af284a8964ccfcdff6",
+  measurementId: "G-8FCVJYZBNF",
 };
 
 // Initialize Firebase
